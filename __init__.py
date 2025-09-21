f32aba11388100a6effa6db1857a4e9b528a5f9d
Vegard Borgeraas <vegard.borgeraas@gmail.com>
2025-09-21 21:37:26 +0200

    chanced comment

--- __init__.py
+++ __init__.py
@@ -4,8 +4,9 @@ from .utils      import *
 
 __all__ = [name for name in globals()]
 
-"""
+
 # hvis jeg begyner å bruke _ for private funcsjoner
+"""
 __all__ = [name for name in globals()
            if not name.startswith("_")]
 """
