5571e9e83dfa79691a5f7c58fd73e3b95eb9d4e1
Vegard Borgeraas <vegard.borgeraas@gmail.com>
2025-10-24 12:00:15 +0200

    moved some things around, added mirror test folder

---
+++ examples/demo.py
@@ -0,0 +1,26 @@
+from custom_matrix_lib_py.core import Matrix
+from sympy import Symbol, Eq, solve, symbols
+
+r = Symbol('r', integer=True)
+λ = Symbol('λ')
+
+A = Matrix([[1, 3],
+            [2, 0]])
+
+P_A = det(A-λ*I(2))
+
+eq  = Eq(P_A, 0)
+print('P_A(λ)=', P_A)
+solutions = solve(eq, λ)
+A, v = symbols('A v', commutative=False)    
+
+for solution in solutions:
+    print(f'λ = {solution}')
+
+R = Matrix([[3, -1],
+            [2, 1]])
+
+D = Matrix([[3**r,       0],
+            [0   , (-2)**r]])
+
+print(5*R*D*~R)
--- matrixlib/mixins/__init__.py
+++ matrixlib/mixins/__init__.py
@@ -1,20 +1,18 @@
-from .binary_operations        import BinaryMatrixOperationsMixin
-from .unary_operations         import UnaryMatrixOperationsMixin
-from .elementary_operations    import ElementaryOperationsMixin
-from .boolean_logic            import BooleanLogicMixin
-from .elementwise_comparison   import ElementwiseComparisonMixin
-from .factory                  import MatrixFactoryMixin
-from .dunder                   import DunderMixin
-from .math_operations          import MatrixMathMixin
-from .epsilon                  import EpsMixin
-from .helper                   import HelperMixin
+from .binary_operations          import BinaryMatrixOperationsMixin
+from .unary_operations           import UnaryMatrixOperationsMixin
+from .elementary_operations      import ElementaryOperationsMixin
+from .boolean_logic              import BooleanLogicMixin
+from .factory                    import MatrixFactoryMixin
+from .dunder                     import DunderMixin
+from .math_operations            import MatrixMathMixin
+from .epsilon                    import EpsMixin
+from .helper                     import HelperMixin
 
 __all__ = [
     "BinaryMatrixOperationsMixin",
     "UnaryMatrixOperationsMixin",
     "ElementaryOperationsMixin",
     "BooleanLogicMixin",
-    "ElementwiseComparisonMixin",
     "MatrixFactoryMixin",
     "DunderMixin",
     "MatrixMathMixin",
